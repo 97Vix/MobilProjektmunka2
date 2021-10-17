24ee8c03c29ae807dc20ee4224261f7481d4be94
csiziszebasztian <tanulas88@gmail.com>
2021-10-17 15:04:37 +0200

    Test AppChart.js

--- components/AppChart.js
+++ components/AppChart.js
@@ -13,24 +13,23 @@ const data = {
         strokeWidth: 2 // optional
       }
     ],
-    legend: ["Rainy Days"] // optional
+    legend: ["Fogyasztás"] // optional
+
   };
 
 const chartConfig = {
     color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 1,
-    useShadowColorFromDataset: false // optional
+    strokeWidth: 200
 };
 
 const screenWidth = Dimensions.get("window").width;
 
 export default function AppChart () {
-    
+
     return (
         <LineChart
             data={data}
-            width={screenWidth*0.6}
+            width={screenWidth*0.58}
             height={256}
             verticalLabelRotation={30}
             chartConfig={chartConfig}
