f5553ad4d487a26330d7588a5118cf351c17bb55
csiziszebasztian <tanulas88@gmail.com>
2021-10-21 14:13:58 +0200

    Make CustomizedPicker

---
+++ components/CustomizedPicker.js
@@ -0,0 +1,30 @@
+import "react-native-gesture-handler";
+
+import React, { useState } from "react";
+
+import { Picker } from "@react-native-picker/picker";
+
+CustomizedPicker = ({handler, interval, selected}) => {
+
+  const [Enable, setEneble] = useState(selected);
+
+  return (
+      <Picker
+        selectedValue={Enable}
+        onValueChange={(itemValue, itemIndex) => { 
+          handler(itemValue);
+          setEneble(itemValue); 
+        }}
+        style={{ height: 25 }}
+        mode="dropdown" // Android only
+      >
+      {   
+          interval.map((item, index)=> (
+            <Picker.Item label={item} value={item} key={index} />
+          ))
+      }
+      </Picker>
+  );
+}
+
+export default CustomizedPicker;
